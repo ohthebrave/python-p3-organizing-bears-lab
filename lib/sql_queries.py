2013987bce8236acceceb5a97dea8b15235a2fb4
George <georgejoj28@gmail.com>
2023-12-06 11:50:57 +0300

    completed

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,9 +1,10 @@
-select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+select_all_female_bears_return_name_and_age =  """
+    SELECT name, age FROM bears WHERE sex = 'female';
 """
 
+
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT * FROM bears ORDER BY name;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
